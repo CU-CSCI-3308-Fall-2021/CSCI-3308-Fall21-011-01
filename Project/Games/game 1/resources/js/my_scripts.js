86d11c0a25b54c4f1236ce097e6e426eb62738b0
dukeman74 <duke.manchester@gmail.com>
2021-11-04 11:48:00 -0600

    pushing more complete game 1

--- Project/Games/game 1/resources/js/my_scripts.js
+++ Project/Games/game 1/resources/js/my_scripts.js
@@ -13,12 +13,21 @@ var rocklist=[
 
 var rocklen=0
 
+var eqlist=[
+  {
+
+  },
+];
+var eqlen=0
+
 var c;
 var ctx;
+const eqfont='40px sans-serif';
 document.addEventListener('keydown', keyDownHandler, false);
 document.addEventListener('keyup', keyUpHandler, false);
 var drawcount=0;
-var rockfrequency=50;
+const icepadding=40;
+var rockfrequency=500;
 var nextrock = 0;
 var rightPressed = false;
 var leftPressed = false;
@@ -26,20 +35,76 @@ var upPressed = false;
 var score = 0;
 var plwidth=10;
 var plheight=50;
+var rxheight=0;
+var rxwidth=0;
 var ded=false;
 var canvaswidth;
 const spdincfrq=1000;
-const spdinc=1;
+const spdinc=0.1;
 var nextspd=0;
-var spd=1;
+var spd=2;
 
-function addrock(rockx)
+function getTextWidth(text, font) {
+  // re-use canvas object for better performance
+  const canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"));
+  const context = canvas.getContext("2d");
+  context.font = font;
+  const metrics = context.measureText(text);
+  return metrics.width;
+}
+function geteq()
 {
-  var rockt=
+  t1=Math.round(Math.random()*9);
+  t2=Math.round(Math.random()*9);
+  ans=t2+t1;
+  dis=String(t1)+ " + " + String(t2);
+  wid= getTextWidth(dis,eqfont)
+  var eqt=
   {
-    x: rockx,
-    y: player.y-400,
+    display: dis,
+    answer: ans,
+    width: wid,
     in: true,
+  }
+  for(var i=0;i<eqlen;i++)
+  {
+    if(eqlist[i].in==false)
+    {
+      eqlist[i]=eqt;
+      return(i)
+    }
+  }
+  eqlist[eqlen]=eqt;
+  eqlen++;
+  return(eqlen-1);
+}
+function addrock(rockx,eq,wid,hit,ind)
+{
+  if(eq)
+  {
+    var rockt=
+    {
+      x: rockx,
+      y: player.y-400,
+      in: true,
+      width: wid,
+      height: hit,
+      eq: true,
+      eqpointer: ind,
+    }
+  }
+  else
+  {
+    var rockt=
+    {
+      x: rockx,
+      y: player.y-400,
+      in: true,
+      eq: false,
+      width: rxwidth,
+      height: rxheight,
+      eqpointer: -1,
+    }
   }
   for(var i=0;i<rocklen;i++)
   {
@@ -52,18 +117,83 @@ function addrock(rockx)
   rocklist[rocklen]=rockt;
   rocklen++;
 }
+function removexrange(x,width)
+{
+  for(i=0;i<rocklen;i++)
+  {
 
+  }
+}
 function addstage()
 {
-  var options = Math.round(Math.random())+2
-  var option1x=Math.round(Math.random()/options)
+  var cutoff;
+  var options = Math.round(Math.random())+2;
+  console.log(options)
+  var ind1=geteq();
+  var opt1w=eqlist[ind1].width+icepadding;
+  var ind2=geteq();
+  var opt2w=eqlist[ind2].width+icepadding;
+  if(options==2)
+  {
+    var optx1=Math.random()*((canvaswidth-opt1w-opt2w)/options);
+    optx1=Math.round(optx1/rxwidth)*rxwidth;
+    var optx2=optx1+opt1w+Math.random()*((canvaswidth-opt1w-opt2w)/options);
+    optx2=Math.round((optx2-(optx1+opt1w))/rxwidth)*rxwidth+optx1+opt1w;
+  }
+  else
+  {
+    var ind3=geteq();
+    var opt3w=eqlist[ind3].width+icepadding;
+    var optx1=Math.random()*((canvaswidth-opt1w-opt2w-opt3w)/options);
+    optx1=Math.round(optx1/rxwidth)*rxwidth;
+    var optx2=optx1+opt1w+Math.random()*((canvaswidth-opt1w-opt2w-opt3w)/options);
+    optx2=Math.round((optx2-(optx1+opt1w))/rxwidth)*rxwidth+optx1+opt1w;
+    var optx3=optx2+opt2w+Math.random()*((canvaswidth-opt1w-opt2w-opt3w)/options);
+    optx3=Math.round((optx3-(optx2+opt2w))/rxwidth)*rxwidth+optx2+opt2w;
+    addrock(optx3,true,opt3w,rxheight,ind3);
+  }
+  addrock(optx1,true,opt1w,rxheight,ind1);
+  addrock(optx2,true,opt2w,rxheight,ind2);
+  cutoff=optx1/rxwidth;
+  for(i=0;i<cutoff;i++)
+  {
+    addrock(i*rxwidth,false);
+  }
+  cutoff=(optx2-(optx1+opt1w))/rxwidth;
+  for(i=0;i<cutoff;i++)
+  {
+    addrock(i*rxwidth+optx1+opt1w,false);
+  }
+  if(options==2)
+  {
+    cutoff=(canvaswidth-(optx2+opt2w))/rxwidth;
+    for(i=0;i<cutoff;i++)
+    {
+      addrock(i*rxwidth+optx2+opt2w,false);
+    }
+  }
+  else
+  {
+    cutoff=(optx3-(optx2+opt2w))/rxwidth;
+    for(i=0;i<cutoff;i++)
+    {
+      addrock(i*rxwidth+optx2+opt2w,false);
+    }
+    cutoff=(canvaswidth-(optx3+opt3w))/rxwidth;
+    for(i=0;i<cutoff;i++)
+    {
+      addrock(i*rxwidth+optx3+opt3w,false);
+    }
+  }
 }
-
 function removerock(id)
 {
   id.in=false;
+  if(id.eq==true)
+  {
+    eqlist[id.eqpointer].in=false;
+  }
 }
-
 function keyDownHandler(event) {
   if(event.keyCode == 39) {
       rightPressed = true;
@@ -98,9 +228,11 @@ function draw() {
   {
     if(player.y<=nextrock)
     {
-      addrock(Math.random()*(canvaswidth-rxwidth));
+      //addrock(Math.random()*(canvaswidth-rxwidth));
+      addstage();
       nextrock-=rockfrequency;
       console.log("rocks: " + rocklen);
+      console.log("equations: "+eqlen);
     }
     if(player.y<=nextspd)
     {
@@ -130,22 +262,52 @@ function draw() {
     ctx.drawImage(plimage,player.x,500)
     for(var i=0;i<rocklen;i++)
     {
-      if(rocklist[i].y-player.y>800)
+      if(rocklist[i].in)
       {
-        removerock(rocklist[i]);
-      }
-      if(rocklist[i].y-player.y+rxheight>=500&&rocklist[i].y-player.y<=500+plheight)
-      {
-        console.log("rock y close");
-        if(rocklist[i].x+rxwidth>=player.x&&rocklist[i].x<=player.x+plwidth)
+        if(rocklist[i].y-player.y>800)
         {
-          console.log("nice throw loser");
-          ded=true;
+          removerock(rocklist[i]);
+        }
+        if(rocklist[i].y-player.y+rocklist[i].height>=500&&rocklist[i].y-player.y<=500+plheight)
+        {
+          console.log("rock y close");
+          if(rocklist[i].x+rocklist[i].width>=player.x&&rocklist[i].x<=player.x+plwidth)
+          {
+            console.log("collision");
+            if(rocklist[i].eq)
+            {
+              if(inpt.value == eqlist[rocklist[i].eqpointer].answer)
+              {
+                removerock(rocklist[i]);
+                inpt.value=""
+              }
+              else
+              {
+                console.log("nice throw loser, correct answer was " + eqlist[rocklist[i].eqpointer].answer + ", you answered: " + inpt.value);
+                ded=true;
+              }
+            }
+            else
+            {
+              console.log("nice throw loser");
+              ded=true;
+            }
+          }
+        }
+        if(rocklist[i].eq==false)
+        {
+          ctx.drawImage(rximage,rocklist[i].x,rocklist[i].y-player.y);
+        }
+        else
+        {
+          ctx.drawImage(iceimage,rocklist[i].x,rocklist[i].y-player.y,rocklist[i].width,rxheight);
+          ctx.font = eqfont;
+          ctx.strokeText(eqlist[rocklist[i].eqpointer].display,rocklist[i].x+icepadding/2,rocklist[i].y-player.y+40)
         }
       }
-      ctx.drawImage(rximage,rocklist[i].x,rocklist[i].y-player.y);
     }
-    ctx.strokeText(Math.abs(player.y),10,20)
+    ctx.font = '10px sans-serif'
+    ctx.strokeText(Math.round(Math.abs(player.y)),10,20)
     window.requestAnimationFrame(draw);
   }
 }
@@ -155,6 +317,7 @@ function drawcanvas()
   c = document.getElementById("myCanvas");
   canvaswidth=c.width;
   ctx = c.getContext("2d");
+  inpt = document.getElementById('input');
   plimage = document.getElementById('playerimage');
   plwidth=plimage.width;
   console.log("player width: " + plwidth);
@@ -165,5 +328,6 @@ function drawcanvas()
   console.log("rock width: " + rxwidth);
   rxheight=rximage.height;
   console.log("rock height: " + rxheight);
+  iceimage=document.getElementById('iceimage');
   draw();
 }
